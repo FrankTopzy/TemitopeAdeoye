6bc4382886f09959b91389d0b85c21d8eca77bb5
FrankTopzy <166184827+FrankTopzy@users.noreply.github.com>
2026-02-25 01:15:20 +0100

    Added some animations to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,30 @@
-import { FaCopyright } from "react-icons/fa6"
+import { FaCopyright } from "react-icons/fa6";
+import { motion } from "framer-motion";
 
 function Footer() {
   const date = new Date();
   const year = date.getFullYear();
 
   return (
-    <div className="flex flex-col w-full text-[var(--navbar-bg)] px-4 py-4 min-h-[10vh] items-center justify-center font-bold">
+    <motion.div className="flex flex-col w-full text-[var(--navbar-bg)] px-4 py-4 min-h-[10vh] items-center justify-center font-bold"
+    initial={{
+      y: 100,
+      opacity: 0,
+      visibility: "hidden"
+    }} 
+    whileInView={{
+      y: 0,
+      opacity: 1,
+      visibility: "visible"
+    }}
+    transition={{
+      duration: 1,
+      ease: "easeInOut"
+    }}>
       <p className="text-center">Built with TypeScript, ReactJs, Vite.js, TailwindCss, Framer Motion, and Vercel.</p>
       <p>Star this project on Github</p>
       <p className="flex items-center gap-2"><FaCopyright/> Frank Topzy {year}</p>
-    </div>
+    </motion.div>
   )
 }
 
